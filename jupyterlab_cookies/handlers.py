7f1168fecf92e83582d4ac99f19ebfa6f3dcd2f6
Josie Garza <josiegarza@google.com>
2020-06-03 20:03:43 +0000

    added auth for project name

--- jupyterlab_cookies/handlers.py
+++ jupyterlab_cookies/handlers.py
@@ -12,7 +12,46 @@ from collections import namedtuple
 from notebook.base.handlers import APIHandler, app_log
 from google.cloud import automl_v1
 
+import google.auth
+from google.auth.exceptions import GoogleAuthError
+from google.auth.transport.requests import Request
+
 from jupyterlab_cookies.version import VERSION
+
+SCOPE = ('https://www.googleapis.com/auth/cloud-platform',)
+
+class AuthProvider:
+  """Provides default GCP authentication credential."""
+
+  _instance = None
+
+  def __init__(self):
+    self._auth, self._project = google.auth.default(scopes=SCOPE)
+
+  @property
+  def project(self):
+    return self._project
+
+  def refresh(self):
+    if not self._auth.valid:
+      app_log.info('Refreshing Google Cloud Credential')
+      try:
+        self._auth.refresh(Request())
+      except GoogleAuthError:
+        msg = 'Unable to refresh Google Cloud Credential'
+        app_log.exception(msg)
+        raise
+
+  def get_header(self):
+    return {'Authorization': 'Bearer {}'.format(self._auth.token)}
+
+  @classmethod
+  def get(cls):
+    if not cls._instance:
+      auth = AuthProvider()
+      cls._instance = auth
+    cls._instance.refresh()
+    return cls._instance
 
 def get_datasets(parent, client):
   datasets = client.list_datasets(parent)
@@ -34,7 +73,7 @@ class ListHandler(APIHandler):
   def get(self, input=''):
     try:
       client = automl_v1.AutoMlClient()
-      parent = client.location_path('[PROJECT]', '[LOCATION]') # Fill in with your project and location
+      parent = client.location_path(AuthProvider.get().project, 'us-central1')
       self.finish(get_datasets(parent, client))
 
     except Exception as e:
